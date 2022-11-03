0794f14af3003b34129c39ef87a84577a1e4c1c2
mayurisamanta <samantamayuri@gmail.com>
2022-11-03 17:13:44 +0530

    adding unit 1 sprint 1

---
+++ Unit 1 Sprint 1 Evaluation/Evaluation U4S1E/src/question3/Main.java
@@ -0,0 +1,15 @@
+package question3;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 64;
+		int b = 70;
+		byte c = (byte) (a+b); //typecast (a+b) to byte
+		System.out.println(c);
+	}
+	
+//	result data type will be according to the following formula:
+//	max (int, type1, type2, type3,...)
+//	according to the above formula, the result data type of the above expression will be “int”
+//	max (int, byte, byte) ——> int
+}
