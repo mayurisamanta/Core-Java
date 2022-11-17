814d55b51b451391cdda8cf63de4748b15c469ba
mayurisamanta <samantamayuri@gmail.com>
2022-11-17 17:20:46 +0530

    ja111 sprint 1

--- Unit 1 Sprint 1 Evaluation/Evaluation U4S1E/src/question4/Main.java
+++ Unit 1 Sprint 1 Evaluation/Evaluation U4S1E/src/question4/Main.java
@@ -3,10 +3,10 @@ package question4;
 public class Main {
 	public static void runScored(int one, int two, int three, int four ,int six) {
 		// write your logic here
-		System.out.println(one + two + three + four + six);
+		System.out.println(one*1 + two*2 + three*3 + four*4 + six*6);
 	}
 	public static void main(String[] args){
 		//call runScored with valid parameters
-		runScored (1,2,3,4,6);
+		runScored (1,1,1,1,1);
 	}
 }
