08df275d591a7a332b8796581e4b09ddfb73d7ae
mayurisamanta <samantamayuri@gmail.com>
2022-12-26 08:23:37 +0530

    ja111

---
+++ Day 5 (Methods , Polymorphism)/U4S2D2 Java methods and polymorphism/src/question2/VowelConsonent.java
@@ -0,0 +1,26 @@
+package question2;
+
+public class VowelConsonent {
+
+	void checkVowelConsonent (char i) {
+		if (i >= 'a' && i <= 'z' || i >= 'A' && i <= 'Z') {
+			if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'A' || i == 'E' || i == 'I' || i == 'O' || i == 'U'){
+				System.out.println(i + " is Vowel");
+			}
+			else {
+				System.out.println(i + " is Consonent");
+			}
+		}
+		else {
+			System.out.println(i + " is Invalid character");
+		}
+	}
+	
+	public static void main(String[] args) {
+		VowelConsonent obj = new VowelConsonent();
+		obj.checkVowelConsonent('e');
+		obj.checkVowelConsonent('L');
+		obj.checkVowelConsonent('&');
+	}
+
+}
